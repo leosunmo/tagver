00cd4fbc5f024fe9967d9090a73b6b6e4ff151ac
Leo Palmer Sunmo <leo.sunmo@gmail.com>
2020-01-23 14:08:08 +1300

    Redo the way we look up tags and print unclean tags

---
+++ git.go
@@ -0,0 +1,137 @@
+/*
+Mostly taken from/inspired by https://github.com/edupo/semver-cli
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/Masterminds/semver"
+	git "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/plumbing/storer"
+)
+
+// Git struct wrapps Repository class from go-git to add a tag map used to perform queries when describing. From https://github.com/edupo/semver-cli/blob/master/gitWrapper/git.go
+type Git struct {
+	TagsMap map[plumbing.Hash][]*plumbing.Reference
+	*git.Repository
+}
+
+// plainOpen opens a git repository from the given path. It detects if the
+// repository is bare or a normal one. If the path doesn't contain a valid
+// repository ErrRepositoryNotExists is returned
+func plainOpen(path string) (*Git, error) {
+	r, err := git.PlainOpen(path)
+	return &Git{
+		make(map[plumbing.Hash][]*plumbing.Reference),
+		r,
+	}, err
+}
+
+func (g *Git) getTagMap() error {
+	tags, err := g.Tags()
+	if err != nil {
+		return err
+	}
+
+	err = tags.ForEach(func(t *plumbing.Reference) error {
+		references := []*plumbing.Reference{t}
+		var hash plumbing.Hash
+		annTag, err := g.TagObject(t.Hash())
+		switch err {
+		case nil:
+			// Annotated Tag object
+			// This means the tags TARGET is actually what we want to use
+			hash = annTag.Target
+			if _, exists := g.TagsMap[hash]; exists {
+				references = append(g.TagsMap[hash], t)
+			}
+		case plumbing.ErrObjectNotFound:
+			// Not an annotated tag object
+			// This means the tags hash is the hash of the actual commit
+			hash = t.Hash()
+			if _, exists := g.TagsMap[hash]; exists {
+				references = append(g.TagsMap[hash], t)
+			}
+		default:
+			// Some other error
+			return err
+		}
+		g.TagsMap[hash] = references
+		return nil
+	})
+
+	return err
+}
+
+// Describe returns the latest tag, number of commits from reference to that tag, and reference's hash.
+// Almost the same as "git describe --tags" other than the "g" prefix on the commit hash
+// If the reference itself has a tag the count and hash will be empty
+func (g *Git) Describe(reference *plumbing.Reference) (string, int, string, error) {
+
+	// Fetch the reference log
+	cIter, err := g.Log(&git.LogOptions{
+		From:  reference.Hash(),
+		Order: git.LogOrderCommitterTime,
+	})
+
+	// Build the tag map
+	err = g.getTagMap()
+	if err != nil {
+		return "", 0, "", err
+	}
+
+	// Search the tag
+	var tag *plumbing.Reference
+	var count int
+	err = cIter.ForEach(func(c *object.Commit) error {
+		if ts, ok := g.TagsMap[c.Hash]; ok {
+			if len(ts) != 1 {
+				tag = getHighestSemverRef(ts)
+			} else {
+				tag = ts[0]
+			}
+		}
+		if tag != nil {
+			return storer.ErrStop
+		}
+		count++
+		return nil
+	})
+	if tag != nil {
+		if count == 0 {
+			return fmt.Sprint(tag.Name().Short()), 0, "", nil
+		}
+		return tag.Name().Short(), count, reference.Hash().String()[0:8], nil
+	}
+	return "", 0, "", nil
+}
+
+func getHighestSemverRef(references []*plumbing.Reference) *plumbing.Reference {
+	if len(references) == 0 {
+		return &plumbing.Reference{}
+	}
+
+	refVer := make(map[string]*plumbing.Reference)
+
+	for _, ref := range references {
+		refVer[ref.Name().Short()] = ref
+	}
+
+	vs := make([]*semver.Version, len(references))
+	for i, r := range references {
+		v, err := semver.NewVersion(r.Name().Short())
+		if err != nil {
+			// One of the strings is not semver formatted, ignore it
+			continue
+		}
+
+		vs[i] = v
+	}
+	sort.Sort(sort.Reverse(semver.Collection(vs)))
+	return refVer[vs[0].Original()]
+}
--- main.go
+++ main.go
@@ -4,45 +4,76 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
-	"sort"
 	"strings"
 
-	"github.com/Masterminds/semver"
 	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
+
+var getTag, getBranch, getCommit, getDefault, ignoreUncleanTag bool
+var path string
 
 // Basic example of how to list tags.
 func main() {
-	var getTag, getBranch, getCommit bool
-	var path string
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of tagver: [-t] [-b] [-c] [<git dir>]\n\n")
+		fmt.Fprint(flag.CommandLine.Output(), "Default output is very close to \"git describe --tags\":\n")
+		fmt.Fprint(flag.CommandLine.Output(), "\tIf HEAD is not tagged: <tag>-<commits since tag>-<HEAD SHA> (example: v1.0.4-1-5227b593)\n")
+		fmt.Fprint(flag.CommandLine.Output(), "\tIf HEAD is tagged: <tag> (example: v1.0.5)\n\n")
+		fmt.Fprint(flag.CommandLine.Output(), "If \"-b\" or \"-c\" are provided with \"-t\", only the tag name will print regardless if it's clean or not.\n")
+		fmt.Fprint(flag.CommandLine.Output(), "Print order will be <tag>-<branch>-<SHA>.\n\n")
+		fmt.Fprintln(flag.CommandLine.Output(), "Set one or more flags.")
+
+		flag.PrintDefaults()
+	}
 
 	flag.BoolVar(&getTag, "t", false, "Return the latest semver tag (annotated or lightweight Git tag) (default)")
 	flag.BoolVar(&getBranch, "b", false, "Return the current branch")
 	flag.BoolVar(&getCommit, "c", false, "Return the current commit")
-	flag.StringVar(&path, "path", "./", "Path of Git repository (Default current directory)")
+	flag.BoolVar(&ignoreUncleanTag, "ignore-unclean-tag", false, "Return only tag name even if the latest tag doesn't point to HEAD (\"v1.0.4\" instead of \"v1.0.4-1-89c22b28\")")
 
 	flag.Parse()
 
 	if !getTag && !getBranch && !getCommit {
-		getTag = true
+		getDefault = true
+	}
+
+	if args := flag.Args(); len(args) != 0 {
+		path = args[0]
+	} else {
+		path = "./"
 	}
 
 	// We instance a new repository targeting the given path (the .git folder)
-	r, err := git.PlainOpen(path)
+	r, err := plainOpen(path)
 	if err != nil {
-		log.Fatalln("Failed to open path")
+		if err == git.ErrRepositoryNotExists {
+			log.Fatalf("Directory %s is not a git repository", path)
+		} else {
+			log.Fatalf("Failed to open git repo %s", path)
+		}
 	}
 
 	var commit, branch, tag string
 
-	if getCommit {
+	if getTag || getDefault {
 		var err error
-		commit, err = getCurrentCommitFromRepository(r)
+		var count int
+		var hash string
+		tag, count, hash, err = getLatestTagFromRepository(r)
 		if err != nil {
-			log.Fatalf("Failed to get current commit from %s, err %s\n", path, err.Error())
+			log.Fatalf("Failed to get latest tag from %s, err %s\n", path, err.Error())
+		}
+		if count != 0 && !ignoreUncleanTag && !getBranch && !getCommit {
+			tag = fmt.Sprintf("%v-%v-%v",
+				tag,
+				count,
+				hash)
+		}
+		if tag == "" && getDefault {
+			getBranch = true
+			getCommit = true
 		}
 	}
 
@@ -54,24 +85,29 @@ func main() {
 		}
 	}
 
-	if getTag {
+	if getCommit {
 		var err error
-		tag, err = getLatestTagFromRepository(r)
+		commit, err = getCurrentCommitFromRepository(r)
 		if err != nil {
-			log.Fatalf("Failed to get latest tag from %s, err %s\n", path, err.Error())
+			log.Fatalf("Failed to get current commit from %s, err %s\n", path, err.Error())
 		}
 	}
+
 	pt := []string{tag, branch, commit}
-	var imageTag []string
+	var versionInfo []string
 	for _, s := range pt {
 		if s != "" {
-			imageTag = append(imageTag, s)
+			versionInfo = append(versionInfo, s)
 		}
 	}
-	fmt.Printf("%s\n", strings.Join(imageTag, "-"))
+	if len(versionInfo) == 0 {
+		log.Println("no version information found")
+		return
+	}
+	fmt.Printf("%s\n", strings.Join(versionInfo, "-"))
 }
 
-func getCurrentBranchFromRepository(repository *git.Repository) (string, error) {
+func getCurrentBranchFromRepository(repository *Git) (string, error) {
 	branchRefs, err := repository.Branches()
 	if err != nil {
 		return "", err
@@ -99,7 +135,7 @@ func getCurrentBranchFromRepository(repository *git.Repository) (string, error) 
 	return strings.TrimPrefix(currentBranchName, "refs/heads/"), nil
 }
 
-func getCurrentCommitFromRepository(repository *git.Repository) (string, error) {
+func getCurrentCommitFromRepository(repository *Git) (string, error) {
 	headRef, err := repository.Head()
 	if err != nil {
 		return "", err
@@ -109,89 +145,17 @@ func getCurrentCommitFromRepository(repository *git.Repository) (string, error) 
 	return headSha, nil
 }
 
-func getLatestTagFromRepository(repository *git.Repository) (string, error) {
-	tagRefs, err := repository.Tags()
+func getLatestTagFromRepository(repository *Git) (string, int, string, error) {
+	// Check if HEAD has tag before iterating over all tags
+	headRef, err := repository.Head()
 	if err != nil {
-		return "", err
+		return "", 0, "", err
 	}
 
-	var tagCandidate *object.Commit
-	err = tagRefs.ForEach(func(tagRef *plumbing.Reference) error {
-		revision := plumbing.Revision(tagRef.Name().String())
-		tagCommitHash, err := repository.ResolveRevision(revision)
-		if err != nil {
-			return err
-		}
-
-		commit, err := repository.CommitObject(*tagCommitHash)
-		if err != nil {
-			return err
-		}
-
-		if tagCandidate == nil {
-			tagCandidate = commit
-		}
-
-		if commit.Committer.When.After(tagCandidate.Committer.When) {
-			tagCandidate = commit
-		}
-
-		return nil
-	})
+	tag, count, latestHash, err := repository.Describe(headRef)
 	if err != nil {
-		return "", err
+		return "", 0, "", err
 	}
-	tagRefs, err = repository.Tags()
-	if err != nil {
-		return "", err
-	}
-	var potentials []string
-	// Special case if there's more than one tag (annotated or lightweight) against a single commit
-	// since the Commiter.When will be the same on both tags, try to find if one has a higher number
-	err = tagRefs.ForEach(func(tagRef *plumbing.Reference) error {
-		revision := plumbing.Revision(tagRef.Name().String())
-		tagCommitHash, err := repository.ResolveRevision(revision)
-		if err != nil {
-			return err
-		}
 
-		commit, err := repository.CommitObject(*tagCommitHash)
-		if err != nil {
-			return err
-		}
-
-		if commit.Hash.String() == tagCandidate.Hash.String() {
-			potentials = append(potentials, tagRef.Name().Short())
-		}
-
-		return nil
-	})
-
-	return getHighestSemver(potentials), nil
-	//return strings.TrimPrefix(latestTagName, "refs/tags/"), nil
-}
-
-func getHighestSemver(semvers []string) string {
-	if len(semvers) == 0 {
-		return ""
-	}
-	vs := make([]*semver.Version, len(semvers))
-	for i, r := range semvers {
-		v, err := semver.NewVersion(r)
-		if err != nil {
-			// One of the strings is not semver formatted, ignore it
-			continue
-		}
-
-		vs[i] = v
-	}
-	sort.Sort(sort.Reverse(semver.Collection(vs)))
-	return vs[0].Original()
-}
-
-func checkArgs(arg ...string) {
-	if len(os.Args) < len(arg)+1 {
-		log.Printf("Usage: %s %s\n", os.Args[0], strings.Join(arg, " "))
-		os.Exit(1)
-	}
+	return tag, count, latestHash, nil
 }
